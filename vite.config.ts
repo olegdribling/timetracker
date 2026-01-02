8640d7b2d3f0e7f8e6f89ed39ce6540a898e53e2
olegdribling <63101414+olegdribling@users.noreply.github.com>
2026-01-03 02:11:22 +1100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,7 @@
-import { defineConfig } from 'vitest/config'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   base: '/timetracker/',
   plugins: [react()],
-  test: {
-    environment: 'node',
-  },
 })
